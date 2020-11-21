333a7eb8fa3cbef37b62ec50a728cdce5731e4d0
amoskorir <amoskrr@gmail.com>
2020-11-21 15:14:46 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,44 +1,19 @@
-# Covista
+# Android cicle pulse Animation
 
-Image Search App- MVVM clean Architecture
+Location, Request, Request Pulse animation.
 
-# Modules
-The application is divided into the following modules (Clean Architecture)
 
-## 1 app
+This a simple implementation of a circular pulse animation using Rxjava3, interval Observable. (alternative to recursive Handler and runnables)
 
-Contains app UI implementation, Activities,fragments and adapters
-
-## 2 Viewmodels
-
-This modules has all the viewmodels tha communicate with the repositories in the datasource module.
-
-## 3 data
-
-This module is a data source, it provide data from api call and local database. It is consumed by the viewmodels
-
-## 4 domain
-
-This a java module that has the app use cases and the utility classes that are used in all the modules
-
-### Using the following Libraries
-
-        1.Koin dependency injection
-
-        2.Rxjava for Streams
-
-        3.Retrofit and Okhttp
-
-        5.Room
 
 <table>
 <tr>
 <td>
-<img  width="200" height="400" src="https://github.com/AmosKorir/covista/blob/master/art/a.png"/>
+<img  width="200" height="400" src="https://github.com/AmosKorir/covista/blob/master/art/a.jpg"/>
 </td>
 <td>
 
-<img  width="200" height="400" src="https://github.com/AmosKorir/covista/blob/master/art/b.png"/>
+<img  width="200" height="400" src="https://github.com/AmosKorir/covista/blob/master/art/b.jpg"/>
 </td>
 </tr>
 </table>
---
+++ app/src/main/java/com/example/pulseanimation/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.example.pulseanimation
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var startBtn: Button
+    private lateinit var stopBtn: Button
+    private lateinit var animationImage: ImageView
+    private lateinit var animationImage2: ImageView
+    private lateinit var animationHandler: Handler
+    private val compositeDisposable = CompositeDisposable()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initView()
+    }
+
+    private fun initView() {
+        startBtn = findViewById(R.id.startBtn)
+        stopBtn = findViewById(R.id.stopBtn)
+        animationImage = findViewById(R.id.backgroundCircle)
+        animationImage2 = findViewById(R.id.backgroundCircle2)
+        animationHandler = Handler(Looper.getMainLooper())
+
+        startBtn.setOnClickListener {
+            startSchedulerAnimation()
+        }
+
+        stopBtn.setOnClickListener {
+            compositeDisposable.clear()
+        }
+    }
+
+    private fun startSchedulerAnimation() {
+        val disposable = Observable.interval(1500, TimeUnit.MILLISECONDS)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .map { runAnimation() }
+            .subscribe(
+                {
+                    Log.e("animationError","success")
+                }, {
+                    Log.e("animationError","Error"+it.message)
+                }
+            )
+        compositeDisposable.add(disposable)
+    }
+
+    private fun runAnimation() {
+        animationImage.animate().scaleX(4f).scaleY(4f).alpha(0f).setDuration(1000).withEndAction {
+            with(animationImage) {
+                scaleX = 1f
+                scaleY = 1f
+                alpha = 1f
+            }
+        }
+        animationImage2.animate().scaleX(4f).scaleY(4f).alpha(0f).setDuration(700).withEndAction {
+            with(animationImage2) {
+                scaleX = 1f
+                scaleY = 1f
+                alpha = 1f
+            }
+        }
+    }
+}
